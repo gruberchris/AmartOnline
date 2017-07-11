825f74b080a7159a305a1652c2416945eec9a8d3
Chris Gruber <chris@chrisgruber.com>
2017-07-10 20:48:23 -0500

    Basic tax service implementation

--- ao-tax-service/index.js
+++ ao-tax-service/index.js
@@ -5,8 +5,28 @@ const app = express();
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('/api/tax/:state', (req, res) => {
+let stateTaxRates = {};
 
+stateTaxRates["IA"] = {state: "IA", rate: 5.0};
+stateTaxRates["IL"] = {state: "IL", rate: 6.0};
+stateTaxRates["MI"] = {state: "MI", rate: 3.0};
+stateTaxRates["MN"] = {state: "MN", rate: 7.5};
+stateTaxRates["WI"] = {state: "WI", rate: 4.25};
+
+app.get('/api/tax', (req, res) => {
+  res.send(stateTaxRates);
+});
+
+app.get('/api/tax/:state', (req, res) => {
+  let state = req.params.state.toUpperCase();
+
+  let stateTaxModel = stateTaxRates[state];
+
+  if(stateTaxModel) {
+    res.send(stateTaxModel);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 const port = process.env.PORT || 5000;
