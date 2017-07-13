8e90c605e4259ac6cdad845a3e9cc997ea6632df
Chris Gruber <chris@chrisgruber.com>
2017-07-12 23:50:36 -0500

    Placeholder for ShoppingCart component

---
+++ amartonline-ui/src/components/shoppingCart/shoppingCart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+  render() {
+    return (
+      <div>
+        <p>Nothing to buy, yet.</p>
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
