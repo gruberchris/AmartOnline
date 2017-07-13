bf9ba44b6bd80324f100e595b43d225669e57ac1
Chris Gruber <chris@chrisgruber.com>
2017-07-12 23:44:34 -0500

    Something to render for home component

--- amartonline-ui/src/components/home/home.js
+++ amartonline-ui/src/components/home/home.js
@@ -1,3 +1,13 @@
-/**
- * Created by chris on 7/12/17.
- */
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Nothing to buy, yet.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
