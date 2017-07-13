b3c2dca1f5de8ec07f283b09b7cae489bff8cfec
Chris Gruber <chris@chrisgruber.com>
2017-07-12 23:48:46 -0500

    Placeholder for UserLogin component

---
+++ amartonline-ui/src/components/userLogin/userLogin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserLogin extends Component {
+  render() {
+    return (
+      <div>
+        <p>Nothing to buy, yet.</p>
+      </div>
+    );
+  }
+}
+
+export default UserLogin;
