cb105ae64d595dd979feec18868c95f7b9dec115
Chris Gruber <chris@chrisgruber.com>
2017-07-12 23:52:25 -0500

    Placeholder for Checkout component

---
+++ amartonline-ui/src/components/checkout/checkout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <div>
+        <p>Nothing to buy, yet.</p>
+      </div>
+    );
+  }
+}
+
+export default Checkout;
