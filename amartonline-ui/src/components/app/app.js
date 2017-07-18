3936d50faccdcbb0e3692745b1a289f1feade54d
Chris Gruber <chris@chrisgruber.com>
2017-07-18 00:06:49 -0500

    Refactored component hierarchy and router config

--- amartonline-ui/src/components/app/app.js
+++ amartonline-ui/src/components/app/app.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom';
 import Header from '../header/header';
+import Home from '../home/home';
+// import Checkout from '../checkout/checkout';
+import ShoppingCart from '../shoppingCart/shoppingCart';
+import Callback from '../callback/callback';
+import UserProfile from '../userProfile/userProfile';
+import Auth from '../../auth/auth';
+
+const auth = new Auth();
 
 class App extends Component {
   render() {
     return (
       <Grid fluid={true}>
-        <Header auth={this.props.auth} />
+        <Header auth={auth} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/callback" render={(props) => {
+            auth.onAuthCallback(props);
+            return <Callback {...props} />
+          }} />
+          <Route exact path="/cart" component={ShoppingCart} />
+          <Route exact path="/profile" component={UserProfile} />
+        </Switch>
       </Grid>
     );
   }
--- amartonline-ui/src/index.js
+++ amartonline-ui/src/index.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/app';
-import Home from './components/home/home';
-// import Checkout from './components/checkout/checkout';
-import ShoppingCart from './components/shoppingCart/shoppingCart';
-import Callback from './components/callback/callback';
-import UserProfile from './components/userProfile/userProfile';
-import Auth from './auth/auth';
 import history from './history';
-
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
-const auth = new Auth();
-
 ReactDOM.render((
   <BrowserRouter history={history}>
-    <div>
-      <Route render={(props) => <App auth={auth} {...props}/>} />
-      <Route exact path="/" render={(props) => <Home {...props}/>} />
-      <Route exact path="/cart" render={(props) => <ShoppingCart {...props}/>} />
-      <Route exact path="/profile" render={(props) => <UserProfile {...props}/>} />
-      <Route path="/callback" render={(props) => {
-        auth.onAuthCallback(props);
-        return <Callback {...props} />
-      }} />
-    </div>
+    <App />
   </BrowserRouter>
 ), document.getElementById('root'));
 
