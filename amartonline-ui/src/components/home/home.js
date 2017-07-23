46df81de29864d427370d6acc3d3018c8d2a7742
Chris Gruber <chris@chrisgruber.com>
2017-07-23 14:02:51 -0500

    Add to cart button should display only when the user is logged on.

--- amartonline-ui/src/components/home/home.js
+++ amartonline-ui/src/components/home/home.js
@@ -78,7 +78,7 @@ class Home extends Component {
         <ListGroupItem key={item.itemId.toString()}>
           <div style={tempButtonContainer}><h4><strong>{item.description}</strong></h4>Only {item.quantity} left on hand. This can be yours for the every day low price of only <strong>${item.price}</strong></div>
           <div style={tempButtonContainer2}>
-            {this.props.auth.authToken &&
+            {this.props.auth.isAuthenticated() &&
             <Button onClick={this.addItemToShoppingCart.bind(this, item)} bsStyle="primary" bsSize="large"><Glyphicon glyph="plus-sign"/>Add To Cart</Button>
             }
           </div>
